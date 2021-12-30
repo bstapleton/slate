0c1d758b04ec5a482a1cecf6dbed6b4ae4ca7b14
Ben Stapleton <webdev.ben@gmail.com>
2021-12-30 14:49:48 +0000

    Tidied up global include
    No longer uses the grid mixin, which will need revisiting and reworking, probably.

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,12 @@ const Home: NextPage = () => {
 
         <div className={'wrapper'}>
           <main>
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+            <h4>Heading 4</h4>
+            <h5>Heading 5</h5>
+            <h6>Heading 6</h6>
             <Card
                 type={'primary'}
                 flex={true}
